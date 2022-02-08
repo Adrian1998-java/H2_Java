1dc84f7e1917bffb5b48c38b005fc4f9d0a4c2d7
PuntunzOfficial <puntunzofficial@gmail.com>
2022-02-08 19:47:33 +0000

    error

--- src/main/java/h2/H2CreateTables.java
+++ src/main/java/h2/H2CreateTables.java
@@ -7,30 +7,29 @@ import java.sql.Statement;
 
 public class H2CreateTables {
 
-	private static final String createTableSQL = "create table users (\r\n" + "  id  int(3) primary key,\r\n" +
-	        "  name varchar(20),\r\n" + "  email varchar(20),\r\n" + "  country varchar(20),\r\n" +
-	        "  password varchar(20)\r\n" + "  );";
+    private static final String createTableSQL = "create table users (\r\n" + "  id  int(3) primary key,\r\n" +
+        "  name varchar(20),\r\n" + "  email varchar(20),\r\n" + "  country varchar(20),\r\n" +
+        "  password varchar(20)\r\n" + "  );";
 
-	    public static void main(String[] argv) throws SQLException {
-	        H2CreateTables createTableExample = new H2CreateTables();
-	        createTableExample.createTable();
-	    }
+    public static void main(String[] argv) throws SQLException {
+        H2CreateTables createTableExample = new H2CreateTables();
+        createTableExample.createTable();
+    }
 
-	    public void createTable() throws SQLException {
+    public void createTable() throws SQLException {
 
-	        System.out.println(createTableSQL);
-	        // Step 1: Establishing a Connection
-	        try (Connection connection = H2JDBCUtils.getConnection();
-	            // Step 2:Create a statement using connection object
-	            Statement statement = connection.createStatement();) {
+        System.out.println(createTableSQL);
+        // Step 1: Establishing a Connection
+        try (Connection connection = H2JDBCUtils.getConnection();
+            // Step 2:Create a statement using connection object
+            Statement statement = connection.createStatement();) {
 
-	            // Step 3: Execute the query or update query
-	            statement.execute(createTableSQL);
+            // Step 3: Execute the query or update query
+            statement.execute(createTableSQL);
 
-	        } catch (SQLException e) {
-	            // print SQL exception information
-	            H2JDBCUtils.printSQLException(e);
-	        }
-	    }
-	
+        } catch (SQLException e) {
+            // print SQL exception information
+            H2JDBCUtils.printSQLException(e);
+        }
+    }
 }
